ef1b4cb11a27332b29203d54bc1188164d8cdc9d
Simon Gustafsson <simongus@yahoo.se>
2024-10-18 11:22:58 +0200

    Add project files.

---
+++ Intro_till_OOP2/Circle.cs
@@ -0,0 +1,16 @@
+﻿namespace Intro_till_OOP2
+{
+    internal class Circle
+    {
+        public double Radius { get; set; }
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public double GetArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+}
---
+++ Intro_till_OOP2/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Intro_till_OOP2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle1 = new Circle(5);
+            double area1 = circle1.GetArea();
+            Console.WriteLine($"Arean för en cirkel med radien 5 är: {area1}");
+
+            Circle circle2 = new Circle(6);
+            double area2 = circle2.GetArea();
+            Console.WriteLine($"Arean för en cirkel med radien 6 är: {area2}");
+        }
+    }
+}
